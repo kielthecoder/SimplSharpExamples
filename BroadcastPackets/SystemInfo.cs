c068269260eed02e627feb30a3717fad9f0e0886
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2023-06-24 10:27:07 -0400

    Missed some files for BroadcastPackets

---
+++ BroadcastPackets/SystemInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BroadcastPackets
+{
+    internal class SystemInfo
+    {
+        public int Sequence { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string HostAddress { get; set; }
+        public int Clock { get; set; }
+    }
+}
